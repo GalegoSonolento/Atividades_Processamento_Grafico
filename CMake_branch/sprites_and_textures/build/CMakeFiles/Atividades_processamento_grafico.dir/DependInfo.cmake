7e0784ff3558aebfad78ad9ccc560ac83f315c46
Henrique H <henriquehaag11@gmail.com>
2024-10-30 18:06:33 -0300

    changes

--- CMake_branch/sprites_and_textures/build/CMakeFiles/Atividades_processamento_grafico.dir/DependInfo.cmake
+++ CMake_branch/sprites_and_textures/build/CMakeFiles/Atividades_processamento_grafico.dir/DependInfo.cmake
@@ -8,13 +8,17 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/henrique/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/glad.c" "CMakeFiles/Atividades_processamento_grafico.dir/src/glad.c.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/glad.c.o.d"
-  "/home/henrique/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/Sprites.cpp" "CMakeFiles/Atividades_processamento_grafico.dir/src/Sprites.cpp.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/Sprites.cpp.o.d"
-  "/home/henrique/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/stb_image/stb_image.cpp" "CMakeFiles/Atividades_processamento_grafico.dir/src/stb_image/stb_image.cpp.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/stb_image/stb_image.cpp.o.d"
+  "/home/haaghenrique/Documents/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/glad.c" "CMakeFiles/Atividades_processamento_grafico.dir/src/glad.c.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/glad.c.o.d"
+  "/home/haaghenrique/Documents/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/Sprites.cpp" "CMakeFiles/Atividades_processamento_grafico.dir/src/Sprites.cpp.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/Sprites.cpp.o.d"
+  "/home/haaghenrique/Documents/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/src/stb_image/stb_image.cpp" "CMakeFiles/Atividades_processamento_grafico.dir/src/stb_image/stb_image.cpp.o" "gcc" "CMakeFiles/Atividades_processamento_grafico.dir/src/stb_image/stb_image.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- CMake_branch/sprites_and_textures/build/cmake_install.cmake
+++ CMake_branch/sprites_and_textures/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/henrique/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures
+# Install script for directory: /home/haaghenrique/Documents/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/henrique/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/haaghenrique/Documents/repos/Atividades_Processamento_Grafico/CMake_branch/sprites_and_textures/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
